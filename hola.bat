7aaaeb2dcfd67b21d312a6e7cdb39c3f9f0651c3
Adrireydepor <84795256+Adrireydepor@users.noreply.github.com>
2022-03-09 17:20:39 +0100

    Add files via upload

---
+++ hola.bat
@@ -0,0 +1,159 @@
+@echo off 
+MODE CON cols=97 lines=15
+echo.
+echo			// C A R G A N D O 	M E N U 	P R I N C I P A L \\		
+echo.
+echo ±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±
+ping localhost -n 2 >nul
+cls
+echo.
+echo			// C A R G A N D O 	M E N U 	P R I N C I P A L \\	
+echo.
+echo ÛÛ±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±
+ping localhost -n 1 >nul
+cls
+echo.
+echo			// C A R G A N D O 	M E N U 	P R I N C I P A L \\	
+echo.
+echo ÛÛÛÛ±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±
+ping localhost -n 1 >nul
+cls
+echo.
+echo			// C A R G A N D O 	M E N U 	P R I N C I P A L \\	
+echo.
+echo ÛÛÛÛÛÛÛ±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±
+ping localhost -n 1 >nul
+cls
+echo.
+echo			// C A R G A N D O 	M E N U 	P R I N C I P A L \\	
+echo.
+echo ÛÛÛÛÛÛÛÛÛÛÛÛ±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±
+ping localhost -n 1 >nul
+cls
+echo.
+echo			// C A R G A N D O 	M E N U 	P R I N C I P A L \\	
+echo.
+echo ÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛ±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±
+ping localhost -n 1 >nul
+cls
+echo.
+echo			// C A R G A N D O 	M E N U 	P R I N C I P A L \\	
+echo.
+echo ÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛ±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±
+ping localhost -n 1 >nul
+cls
+echo.
+echo			// C A R G A N D O 	M E N U 	P R I N C I P A L \\	
+echo.
+echo ÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛ±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±
+ping localhost -n 1 >nul
+cls
+echo.
+echo			// C A R G A N D O 	M E N U 	P R I N C I P A L \\	
+echo.
+echo ÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛ±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±
+ping localhost -n 1 >nul
+cls
+echo.
+echo			// C A R G A N D O 	M E N U 	P R I N C I P A L \\	
+echo.
+echo ÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛ±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±±
+ping localhost -n 1 >nul
+cls
+echo.
+echo			// C A R G A N D O 	M E N U 	P R I N C I P A L \\	
+echo.
+echo ÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛ±±±±±±±±±±±±±±±±±±
+ping localhost -n 1 >nul
+cls
+echo.
+echo			// C A R G A N D O 	M E N U 	P R I N C I P A L \\	
+echo.
+echo ÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛ±±±±±±±
+ping localhost -n 1 >nul
+cls
+echo.
+echo			// C A R G A N D O 	M E N U 	P R I N C I P A L \\	
+echo.
+echo ÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛÛ
+ping localhost -n 1 >nul		
+cls
+echo.
+echo.
+echo			      \\  F R E E      K U B A      C H E A T S  //
+ECHO.
+ECHO.
+ECHO.
+ECHO										   discord.gg													
+pause
+cls
+echo checkeando status...
+echo status:
+curl https://raw.githubusercontent.com/Adrireydepor/status/main/version.txt
+echo last version:
+curl https://raw.githubusercontent.com/Adrireydepor/status/main/last-version.txt
+echo current version:
+echo 1.0 release 09/03/2022
+pause
+MODE CON cols=132 lines=16
+:menuv1
+title discord.gg
+color 1f
+cls
+echo.  
+echo '-..-''-..-''-..-''-..-''-..-''-..-''-..-''-..-''-..-''-..-''-..-''-..-''-..-''-..-''-..-''-..-''-..-''-..-''-..-''-..-''-..-'.-'   
+echo '-..-''-..-''-..-''-..-''-..-''-..-''-..-''-..-''-..-''-..-''-..-''-..-''-..-''-..-''-..-''-..-''-..-''-..-''-..-''-..-''-..-'.-'
+echo.										    
+echo 						        BIENVENIDO AL MENU PRINCIPAL 		
+echo 								   v1.0B	    	        	
+echo '-..-''-..-''-..-''-..-''-..-''-..-''-..-''-..-''-..-''-..-''-..-''-..-''-..-''-..-''-..-''-..-''-..-''-..-''-..-''-..-''-..-'.-'
+echo.
+echo.
+echo 			1. Instalar hacks 
+echo 			2. Actualizar hacks
+echo 			3. inyectar candy
+echo                    4. Desinstalar menu y hacks
+echo 			5. Salir
+echo.       	   
+set/p var= Selecciona una opcion 
+ 
+ 
+if %var%==1 goto installer
+if %var%==2 goto actualizar
+if %var%==3 goto inyectar
+if %var%==4 goto desinstalar
+if %var%==5 goto d
+
+
+:installer
+color 1f
+cls
+curl -o menu.zip https://codeload.github.com/Adrireydepor/how-is-this/zip/refs/heads/main
+tar -xf menu.zip
+Robocopy how-is-this-main data /move /s /w:1 /r:1
+move "data\candy" "C:\"
+cls
+del menu.zip
+cls
+pause
+goto menuv1
+
+:actualizar
+color 1f
+cls
+updater.bat
+pause
+goto menuv1
+
+:inyectar
+data\loader.exe
+exit
+
+:desinstalar
+del data /f /q
+del updater.bat /f /q
+del c:\candy /f /q
+del hola.bat /f /q
+
+:d
+exit
